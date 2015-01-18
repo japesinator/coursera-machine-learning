319ce0da0fdad0948a959ef159ca66d9f9ffead0
JP Smith <jp.smith@ilstu.edu>
2015-01-17 21:07:05 -0600

    regularized

--- mlclass-ex5-006/mlclass-ex5/linearRegCostFunction.m
+++ mlclass-ex5-006/mlclass-ex5/linearRegCostFunction.m
@@ -20,7 +20,11 @@ grad = zeros(size(theta));
 %
 
 
-J = (1/(2*m)) * sum(((X * theta) - y).^2) + (lambda / (2*m)) * (theta(2:end)' * theta(2:end));
+J = (1/(2*m)) * sum(((X * theta) - y).^2) + (lambda / (2*m)) ...
+  * (theta(2:end)' * theta(2:end));
+
+grad         = (X' * ((X * theta) - y)) / m;
+grad(2:end) += lambda * theta(2:end) / m;
 
 
 
